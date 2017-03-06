e16b6c4a17110a6411c3cef0421c56b4f757a171
João Palhinha <joao@spic.pt>
2017-03-06 20:19:04 +0000

    layout pagina inicia câmera

--- app/src/main/java/spic/nau_btl/MainActivity.java
+++ app/src/main/java/spic/nau_btl/MainActivity.java
@@ -1,13 +1,63 @@
 package spic.nau_btl;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private static final int REQUEST_WRITE_STORAGE = 112;
+    public static final int MY_PERMISSIONS_REQUEST_CAMERA = 42;
+    private int contador = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        boolean hasPermission = (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED);
+        if (!hasPermission) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_WRITE_STORAGE);
+        }
+
+        boolean hasPermission2 = (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED);
+
+        if (!hasPermission2) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.CAMERA},
+                    MY_PERMISSIONS_REQUEST_CAMERA);
+        }
+
+
+        Button buttonTira= (Button)findViewById(R.id.buttonIniciarCamera);
+
+        buttonTira.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View arg0) {
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA)
+                        == PackageManager.PERMISSION_GRANTED) {
+//                    Intent nextActivity = new Intent(MainActivity.this, TirarFotoActivity.class);
+//                    startActivity(nextActivity);
+                } else {
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.CAMERA},
+                            MY_PERMISSIONS_REQUEST_CAMERA);
+                }
+
+            }});
+
+
     }
+
+
 }
